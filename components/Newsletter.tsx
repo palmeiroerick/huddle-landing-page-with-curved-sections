1907aabc6256482d0c8ba6fbccc9442d57a02a1d
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-13 17:49:36 -0300

    style: add desktop styles to newsletter component

--- components/Newsletter.tsx
+++ components/Newsletter.tsx
@@ -4,16 +4,24 @@ import Title from "./Title";
 
 export default function Newsletter() {
   return (
-    <div className="flex flex-col gap-6">
-      <Title as="h2" className="self-start text-white font-semibold uppercase text-xl">
+    <section className="flex flex-col gap-6 lg:w-[33rem]">
+      <Title
+        as="h2"
+        className="self-start text-xl font-semibold uppercase text-white"
+      >
         Newsletter
       </Title>
-      <Text className="text-start text-white text-sm">
+      <Text className="text-start text-sm text-white lg:w-[22.5rem]">
         To receive tips on how to grow your community, sign up to our weekly
         newsletter. We’ll never send you spam or pass on your email address
       </Text>
-      <input type="text" className="h-12 rounded-lg outline-none text-veryDarkCyan px-3" />
-      <Button type="Subscribe" />
-    </div>
+      <div className="flex flex-col gap-6 lg:flex-row">
+        <input
+          type="text"
+          className="h-12 rounded-lg px-3 text-veryDarkCyan outline-none lg:w-[21.5rem]"
+        />
+        <Button type="Subscribe" />
+      </div>
+    </section>
   );
 }
