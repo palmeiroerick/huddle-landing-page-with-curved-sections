e4c91d5a71f917935c2746249e2e183eb191dbbf
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-13 17:49:19 -0300

    style: add desktop styles to getstarted component

--- components/GetStarted.tsx
+++ components/GetStarted.tsx
@@ -3,8 +3,8 @@ import Title from "./Title";
 
 export default function GetStarted() {
   return (
-    <div className="pb-64 pt-32 flex flex-col gap-8">
-      <Title as="h2" className="font-bold font-openSans">Ready To Build Your Community?</Title>
+    <div className="pb-64 pt-32 flex flex-col gap-8 lg:py-44">
+      <Title as="h2" className="font-bold font-openSans lg:text-3xl">Ready To Build Your Community?</Title>
       <Button type="Get Started For Free" />
     </div>
   );
