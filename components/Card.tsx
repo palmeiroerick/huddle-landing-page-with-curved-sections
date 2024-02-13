4bd6641beab4594b464791194e6ec152adffb8cd
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-13 17:49:09 -0300

    style: add desktop styles to card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -9,9 +9,9 @@ type CardProps = {
 
 export default function Card({ type, children }: CardProps) {
   const styles = {
-    "Grow Together": "bg-veryPaleBlue",
-    "Flowing Conversations": "bg-white",
-    "Your Users": "bg-veryPaleBlue",
+    "Grow Together": "bg-veryPaleBlue lg:flex-row-reverse",
+    "Flowing Conversations": "bg-white lg:flex-row",
+    "Your Users": "bg-veryPaleBlue lg:flex-row-reverse",
   };
 
   const imageSrc = {
@@ -22,15 +22,22 @@ export default function Card({ type, children }: CardProps) {
 
   return (
     <div
-      className={`${styles[type]} relative z-10 flex flex-col items-center gap-4 px-7 py-16`}
+      className={`${styles[type]} relative z-10 flex flex-col items-center gap-4 px-7 py-16 lg:justify-between lg:px-28`}
     >
-      <div className="relative mb-7 h-56 w-64">
+      <div className="relative mb-7 h-56 w-64 lg:h-[27rem] lg:w-[32.5rem]">
         <Image className="object-contain" src={imageSrc[type]} alt="" fill />
       </div>
-      <Title as="h2" className="font-openSans text-xl font-bold">
-        {type}
-      </Title>
-      <Text className="font-poppins text-[13px] ">{children}</Text>
+      <div className="flex flex-col gap-4 lg:w-[32.5rem]">
+        <Title
+          as="h2"
+          className="font-openSans text-xl font-bold lg:text-start lg:text-3xl"
+        >
+          {type}
+        </Title>
+        <Text className="font-poppins text-[13px] lg:text-start">
+          {children}
+        </Text>
+      </div>
     </div>
   );
 }
