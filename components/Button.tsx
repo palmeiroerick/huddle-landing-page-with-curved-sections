29349f5e7a4070cc8583438f82375a36a06e09c8
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-13 17:48:01 -0300

    style: add desktop styles to button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,14 +5,14 @@ type ButtonProps = {
 export default function Button({ type }: ButtonProps) {
   const styles = {
     "Get Started For Free":
-      "bg-pink text-white h-10 w-52 rounded-full text-sm self-center",
+      "bg-pink text-white h-10 w-52 rounded-full text-sm self-center lg:w-80 lg:h-16",
     "Try it Free":
-      "h-6 w-24 rounded-full text-pink border border-pink text-xs self-center",
+      "h-6 w-24 rounded-full text-pink border border-pink text-xs self-center lg:h-10 lg:w-28",
     Subscribe: "h-12 w-40 rounded-md bg-pink text-white self-end",
   };
 
   return (
-    <button className={`font-openSans font-bold ${styles[type]}`}>
+    <button className={`font-openSans font-bold ${styles[type]} lg:w`}>
       {type}
     </button>
   );
