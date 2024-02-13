46600bf7e6604fb19091ab694360f8bed265ee3f
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-13 17:48:48 -0300

    style: add desktop styles to mockups component

--- components/Mockups.tsx
+++ components/Mockups.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Mockups() {
   return (
-    <div className="relative m-auto h-52 w-72">
+    <div className="relative m-auto h-52 w-72 lg:h-[44rem] lg:w-[61rem]">
       <Image
         className="object-contain"
         src="/screen-mockups.svg"
